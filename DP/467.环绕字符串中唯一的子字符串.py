def0900e93087ea633e6c48fe9ab898474cff9cf
boredcui <1637188453@qq.com>
2022-05-25 21:18:12 +0800

    day 72

---
+++ DP/467.环绕字符串中唯一的子字符串.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: boredcui 1637188453@qq.com
+Date: 2022-05-25 21:18:04
+LastEditors: boredcui 1637188453@qq.com
+LastEditTime: 2022-05-25 21:18:06
+FilePath: \LeetCode\DP\467.环绕字符串中唯一的子字符串.py
+Description: 
+
+Copyright (c) 2022 by boredcui 1637188453@qq.com, All Rights Reserved. 
+'''
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: boredcui 1637188453@qq.com
+Date: 2022-05-25 21:14:28
+LastEditors: boredcui 1637188453@qq.com
+LastEditTime: 2022-05-25 21:17:45
+FilePath: \LeetCode\467.环绕字符串中唯一的子字符串.py
+Description: 
+
+Copyright (c) 2022 by boredcui 1637188453@qq.com, All Rights Reserved. 
+'''
+#
+# @lc app=leetcode.cn id=467 lang=python3
+#
+# [467] 环绕字符串中唯一的子字符串
+#
+
+# @lc code=start
+
+
+class Solution:
+    def findSubstringInWraproundString(self, p: str) -> int:
+        dp = defaultdict(int)
+        k = 0
+        for i, ch in enumerate(p):
+            if i > 0 and (ord(ch) - ord(p[i - 1])) % 26 == 1:  # 字符之差为 1 或 -25
+                k += 1
+            else:
+                k = 1
+            dp[ch] = max(dp[ch], k)
+        return sum(dp.values())
+# @lc code=end
