05902c28beb4370e7b02a65a0fcbf679cbe64819
boredcui <1637188453@qq.com>
2022-07-01 23:37:25 +0800

    day 100

---
+++ Medium/241.为运算表达式设计优先级.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: boredcui 1637188453@qq.com
+Date: 2022-07-01 23:36:30
+LastEditors: boredcui 1637188453@qq.com
+LastEditTime: 2022-07-01 23:37:01
+FilePath: \LeetCode\241.为运算表达式设计优先级.py
+Description: 
+
+Copyright (c) 2022 by boredcui 1637188453@qq.com, All Rights Reserved. 
+'''
+#
+# @lc app=leetcode.cn id=241 lang=python3
+#
+# [241] 为运算表达式设计优先级
+#
+
+# @lc code=start
+ADDITION = -1
+SUBTRACTION = -2
+MULTIPLICATION = -3
+
+
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        ops = []
+        i, n = 0, len(expression)
+        while i < n:
+            if expression[i].isdigit():
+                x = 0
+                while i < n and expression[i].isdigit():
+                    x = x * 10 + int(expression[i])
+                    i += 1
+                ops.append(x)
+            else:
+                if expression[i] == '+':
+                    ops.append(ADDITION)
+                elif expression[i] == '-':
+                    ops.append(SUBTRACTION)
+                else:
+                    ops.append(MULTIPLICATION)
+                i += 1
+
+        @cache
+        def dfs(l: int, r: int) -> List[int]:
+            if l == r:
+                return [ops[l]]
+            res = []
+            for i in range(l, r, 2):
+                left = dfs(l, i)
+                right = dfs(i + 2, r)
+                for x in left:
+                    for y in right:
+                        if ops[i + 1] == ADDITION:
+                            res.append(x + y)
+                        elif ops[i + 1] == SUBTRACTION:
+                            res.append(x - y)
+                        else:
+                            res.append(x * y)
+            return res
+        return dfs(0, len(ops) - 1)
+
+# @lc code=end
